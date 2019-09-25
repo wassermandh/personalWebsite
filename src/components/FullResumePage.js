7e64bb9db23de7ff3e6befbc9b03567f1a8a88f3
Daniel Wasserman <daniel.h.wasserman@gmail.com>
2019-09-24 20:07:16 -0400

    fixed resume path

--- src/components/FullResumePage.js
+++ src/components/FullResumePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Resume from './Resume';
+const resume = require('./misc/Daniel_Wasserman.pdf');
 
 const FullResumePage = props => {
   return (
     <div id="resumeContainer">
-      <Link to="./misc/Daniel_Wasserman.pdf" target="_blank" download>
+      <Link to={resume} target="_blank" download>
         <h2>Click to Download</h2>
       </Link>
       <Resume />
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${
-  pdfjs.version
-}/pdf.worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+const resume = require('./misc/Daniel_Wasserman.pdf');
 
 const Resume = props => {
   return (
-    <Document file="../../misc/Daniel_Wasserman.pdf">
+    <Document file={resume}>
       <Page pageNumber={1} scale={2} />
     </Document>
   );
